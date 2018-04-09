5222f85900bcb5d8ca192c080c7cb93a3f08fa54
Yishen Miao <mys721tx@gmail.com>
2018-04-09 15:23:23 -0400

    Implementing FASTA reader
    Implements FASTA reader for sequences.

--- gsearch/gsearch.go
+++ gsearch/gsearch.go
@@ -11,17 +11,19 @@ import (
 	"fmt"
 	"github.com/biogo/biogo/align"
 	"github.com/biogo/biogo/alphabet"
+	"github.com/biogo/biogo/io/seqio/fasta"
 	"github.com/biogo/biogo/seq/linear"
 	"log"
+	"os"
 )
 
 var (
-	reference = flag.String("reference", "", "reference sequence to align")
-	target    = flag.String("target", "", "target sequence to align")
-	match     = flag.Int("match", 2, "score for match")
-	mismatch  = flag.Int("mismatch", -1, "score for mismatch")
-	gap       = flag.Int("gap", -2, "score for gap")
-	gapopen   = flag.Int("gap_open", 0, "score for gap open")
+	ref      = flag.String("reference", "", "reference sequence to align")
+	tgt      = flag.String("target", "", "target sequence to align")
+	match    = flag.Int("match", 2, "score for match")
+	mismatch = flag.Int("mismatch", -1, "score for mismatch")
+	gap      = flag.Int("gap", -2, "score for gap")
+	gapopen  = flag.Int("gap_open", 0, "score for gap open")
 )
 
 func makeScoreMatrix() *align.Linear {
@@ -35,26 +37,51 @@ func makeScoreMatrix() *align.Linear {
 	return &m
 }
 
+func readSeq(f *os.File) *linear.Seq {
+
+	r := fasta.NewReader(f, linear.NewSeq("", nil, alphabet.DNAgapped))
+
+	s, err := r.Read()
+
+	if err != nil {
+		log.Fatalf("failed to read sequence: %v", err)
+	}
+
+	// Type assertion to linear.Seq
+	return s.(*linear.Seq)
+}
+
 func main() {
 	flag.Parse()
 
-	seqReference := &linear.Seq{Seq: alphabet.BytesToLetters([]byte(*reference))}
-	seqReference.Alpha = alphabet.DNAgapped
-	seqTarget := &linear.Seq{Seq: alphabet.BytesToLetters([]byte(*target))}
-	seqTarget.Alpha = alphabet.DNAgapped
+	fRef, err := os.Open(*ref)
+
+	if err != nil {
+		log.Fatalf("failed to open %q: %v", *ref, err)
+	}
+
+	sRef := readSeq(fRef)
+
+	fTgt, err := os.Open(*tgt)
+
+	if err != nil {
+		log.Fatalf("failed to open %q: %v", *ref, err)
+	}
+
+	sTgt := readSeq(fTgt)
 
 	smith := align.SWAffine{
 		Matrix:  *makeScoreMatrix(),
 		GapOpen: *gapopen,
 	}
 
-	aln, err := smith.Align(seqReference, seqTarget)
+	aln, err := smith.Align(sRef, sTgt)
 
 	if err != nil {
 		log.Fatalf("failed to align: %v", err)
 	}
 
 	fmt.Printf("%s\n", aln)
-	fa := align.Format(seqReference, seqTarget, aln, '-')
+	fa := align.Format(sRef, sTgt, aln, '-')
 	fmt.Printf("%s\n%s\n", fa[0], fa[1])
 }
